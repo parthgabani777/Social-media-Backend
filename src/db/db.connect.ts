93b4682d6d050bb8abb8811880f950e3413f3168
Parth Gabani <gabaniparth04@gmail.com>
2022-09-30 20:44:20 +0530

    feat: added docker compose file

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -4,7 +4,7 @@ export async function dbConnect() {
     const dbUrl = process.env.DB_URL;
     try {
         if (dbUrl) {
-            await mongoose.connect(dbUrl);
+            await mongoose.connect(dbUrl, { dbName: "social-media" });
             console.log("Database connected");
         }
     } catch (error) {
